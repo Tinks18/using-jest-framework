e0c170d3fc7f2cef2a407e7901175036adf0ba7f
Tinks18 <tinutani9@gmail.com>
2023-01-15 18:56:55 +0000

    Add function

--- scripts/test/calc.js
+++ scripts/test/calc.js
@@ -1,18 +1,5 @@
-const addition = require("./calc");
+function addition() {
+    return 42;
+}
 
-describe("Calculator", () => {
-    describe("Addition function", () => {
-        test("should return 42 for 20 + 22", () => {
-            expect(addition(20, 22)).toBe(42);
-        });
-    });
-    describe("Subtract function", () => {
-
-    });
-    describe("Multiply function", () => {
-
-    });
-    describe("Divide function", () => {
-
-    });
-});
+module.exports = addition;
